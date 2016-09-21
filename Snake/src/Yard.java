3fa9dae223da5fff22714bf03453e392b17779a4
jimmygmao <892458803@qq.com>
2016-09-22 00:11:57 +0800

    设置了窗口

--- Snake/src/Yard.java
+++ Snake/src/Yard.java
@@ -1,4 +1,32 @@
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-public class Yard {
 
+public class Yard extends Frame{
+	private static final int LINE=50;
+	private static final int VERTICAL=50;
+	private static final int CELL=10;
+	public static void main(String[] args){
+		new Yard().PageTable();
+	}
+	public void PageTable(){
+		this.setLocation(300, 200);
+		this.setSize(LINE*CELL, VERTICAL*CELL);
+		this.setVisible(true);
+		this.setResizable(false);
+		this.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e){
+				System.exit(0);
+			}
+		});
+	}
+	public void paint(Graphics g){
+		Color c=g.getColor();
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, LINE*CELL, VERTICAL*CELL);
+		g.setColor(c);
+	}
 }
