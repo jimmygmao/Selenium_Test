2d12b5bc9f79e81988192d13b86560f38a9aeb40
jimmygmao <892458803@qq.com>
2017-01-10 23:43:56 +0800

    Mouse

--- selenium/src/LoginQq.java
+++ selenium/src/LoginQq.java
@@ -1,6 +1,5 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LoginQq {
@@ -8,14 +7,20 @@ public class LoginQq {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-/*		driver.get("https://www.wxw7z.com/");
-		driver.findElement(By.linkText("µÇÂ¼")).click();
-		driver.findElement(By.id("phone")).sendKeys("13549893438");
-		driver.findElement(By.id("pwd")).sendKeys("123456");
-		driver.findElement(By.className("u_logo")).click();*/
-		
-		driver.get("https://ssl.zc.qq.com/chs/index.html");
-		driver.findElement(By.id("nick")).sendKeys("I Love You");
-		driver.findElement(By.id("nick")).submit();
+		driver.get("https://mail.qq.com/");
+		driver.switchTo().frame("login_frame");
+		driver.findElement(By.id("u")).clear();
+		driver.findElement(By.id("u")).sendKeys("892458803");
+		driver.findElement(By.id("p")).clear();
+		driver.findElement(By.id("p")).sendKeys("386166631070707");
+		driver.findElement(By.className("login_button")).click();
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		driver.navigate().refresh();
+		driver.findElement(By.id("pp")).sendKeys("dgm0218");
+		driver.findElement(By.id("pp")).submit();
 	}
 }
