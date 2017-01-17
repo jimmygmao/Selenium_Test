6cc0844092f640043fef45b2493306b8d6ad9ae1
jimmygmao <892458803@qq.com>
2017-01-17 23:53:29 +0800

    Ifame,FindElements,Windows(50%)

---
+++ selenium/src/TestIframe.java
@@ -0,0 +1,22 @@
+import java.io.File;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class TestIframe {
+	public static void main(String[] args){
+		System.setProperty("webdriver.chrome.driver", "C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		File file=new File("../selenium/src/Ifame.html");
+		String filePath=file.getAbsolutePath();
+		driver.get(filePath);
+		WebElement vf=driver.findElement(By.xpath("//iframe[@id='if']"));
+		driver.switchTo().frame(vf);
+		driver.findElement(By.id("kw")).sendKeys("hehe");
+		//退回上一级表单
+		driver.switchTo().defaultContent();
+	}
+}
